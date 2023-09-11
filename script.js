727f33b64ee9ec25de4809ab3d62ae8a9abdbadb
unknown <abeltran9@mail.valenciacollege.edu>
2023-09-10 21:41:30 -0400

    Added text file to repo

---
+++ script.js
@@ -0,0 +1,9 @@
+let firstName = "My first name is ";
+    firstName = firstName + "Alonso";
+let lastName = "My last name is ";
+    lastName = lastName + "Beltran";
+const myAge = "I am [AGE] years old";
+    let [AGE] = "28";
+alert("My name is Alonso Beltran");
+alert("I am 28 years old");
+console.log("I am enjoying learning in this class! Thank you!");
